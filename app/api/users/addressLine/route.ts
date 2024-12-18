a8e18188a3066263071625ee69fb11f4033e61b4
Emmanuel Aguado <aguadojr.emmanueldiaz@gmail.com>
2024-12-18 12:55:33 +0800

    removed api routes

--- app/api/users/addressLine/route.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "@/lib/db";
-import { NextResponse } from "next/server";
-
-export async function GET(request: Request) {
-    const allAddresses = await db.addressLine.findMany();
-
-
-    return NextResponse.json(allAddresses);
-}
--- app/api/users/route.ts
+++
@@ -1,8 +0,0 @@
-import { db } from "@/lib/db";
-import { NextResponse } from "next/server";
-
-export async function GET(request: Request) {
-    const allUsers = await db.user.findMany();
-    // console.log(allUsers);
-    return NextResponse.json(allUsers);
-}   
